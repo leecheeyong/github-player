8c9a52e4b7f1d1c528030b8cb48d576bc20a6ce8
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-10-23 00:14:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function run() {
         downloaded.push(dir[i].slice(0, -4));
     }      
     await wait(5000);  
-    const data = (await search.getPlaylist("PLME6vJ3ZxCkF7w").then(playlist => playlist.fetch()));
+    const data = (await search.getPlaylist("PLME6vJ3ZxCkF7w-Vm9KVelquVoeQgt8jP").then(playlist => playlist.fetch()));
     if(!Array.isArray(data?.videos)) throw new Error("No videos found"); 
      var videos = data.videos;  
      for(i in videos) { 
