b7946c809a656e86a1c22d98f4fc8ee846f30d2d
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-10-22 20:49:19 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,100 @@
+// handcoded by Chee Yong Lee
+// view license https://github.com/CodingStudios/google-drive-player
+const playlist = document.getElementById("playlist");
+const slidebar = document.getElementById("slidebar");
+const pauseButton = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgba(255, 255, 255, 255);"><path d="M8 7h3v10H8zm5 0h3v10h-3z"></path></svg>';
+const playButton = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgba(255,255,255,255);"><path d="M7 6v12l10-6z"></path></svg>'
+var currentTrack;
+var list = [];
+var timestamp = document.getElementById("timestamp");
+const audio = new Audio();
+const control = document.getElementById("control");
+var page = 0;
+var currentTrackName = document.getElementById("nowPlaying");
+
+function playAudio(track) {
+    audio.src = `/music/${track}.mp3`;
+    currentTrack = track;
+    currentTrackName.textContent = name.slice(0, -4);
+    audio.play();
+    control.innerHTML = pauseButton;
+    playing = true;
+}
+
+window.onload = () => {
+var playing = false;
+fetchPlaylist();
+function fetchPlaylist() {
+fetch(`/playlist.json`).then(r=>r.json()).then(r=> {
+    list = list.concat(r.list);
+    page = r.page;
+    r.list.forEach(e => {
+        var music = document.createElement("div");
+        music.textContent = e.name;
+        music.onclick = () => {
+            playAudio(e.trackNumber, e.name);
+        }     
+        music.classList.add("music");
+       playlist.appendChild(music);
+    })
+});
+}
+playlist.onscroll = () => {
+    if(playlist.scrollTop + playlist.clientHeight >= playlist.scrollHeight) {
+        fetchPlaylist();
+    }
+}
+control.addEventListener("click", () => {
+    if(!list) return;
+    if(!currentTrack && !currentTrackName) return playAudio(1, list[0].name);
+    if (!playing) {
+        playing = true;
+        audio.play();
+        control.innerHTML = pauseButton;
+    } else {
+        playing = false;
+        audio.pause();
+        control.innerHTML = playButton;
+    }
+})
+
+audio.addEventListener("ended", () => {
+    if(currentTrack == list.length) return;
+    playAudio(currentTrack + 1, list[currentTrack].name);
+})
+
+document.getElementById("previous").addEventListener("click", () => {
+    if(!list) return;
+    if(!currentTrack && !currentTrackName) return playAudio(1, list[0].name);
+    if(currentTrack == 1) return;
+    playAudio(currentTrack - 1, list[currentTrack - 2].name);
+});
+
+document.getElementById("next").addEventListener("click", () => {
+    if(!list) return;
+    if(!currentTrack && !currentTrackName) return playAudio(1, list[0].name);
+    if(currentTrack == list.length) return;
+    playAudio(currentTrack + 1, list[currentTrack].name);
+});
+
+audio.ontimeupdate = function () {
+    timestamp.textContent = timeFormat(audio.currentTime);
+   slidebar.value = audio.currentTime / audio.duration * 100;
+}
+
+slidebar.oninput = (e) => {
+  audio.currentTime = audio.duration / 100 * slidebar.value, audio.duration;
+}
+
+function timeFormat(ct) {
+    minutes = Math.floor(ct / 60);
+    seconds = Math.floor(ct % 60);
+  
+    if (seconds < 10) {
+      seconds = "0"+seconds;
+    }
+  
+    return minutes + ":" + seconds;
+}
+
+}
