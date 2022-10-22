359f9171aa758cdffc116a455469f7bde4bdf244
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-10-22 20:58:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const fs = require('fs');
 const yts = require('yt-search');
 const ffmpeg = require('fluent-ffmpeg');
 const { exec } = require('child_process');
-var content = ``;
+const content = [];
 
 const music = fs.readdirSync('./music').filter(file => file.endsWith('.mp3'));
-const html = fs.readFileSync('./index.html');
 
 music.forEach(async (file) => {
   try {
     const video = await yts(file.slice(0, -4));
     const stream = ytdl(video?.all[0].url, { filter: 'audioonly' });
-    content += `<audio controls src="./music/${file}"></audio>`;
-    fs.writeFileSync("./index.html", content)
+    content.push({
+      name: `${video?.all[0]?.title}`,
+      track: `/music/${video?.all[0]?.id}`
+    });
+    fs.writeFileSync("./playlist.json", content)
     const stats = fs.statSync(`./music/${file}`);
     const fileSizeInBytes = stats.size;
     const size = fileSizeInBytes / (1024*1024);
