76f78473ff21be9d349104f0bdcc0330dfff807a
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-12-28 15:28:43 +0800

    Update chinese-lyrics.js

--- chinese-lyrics.js
+++ chinese-lyrics.js
@@ -50,7 +50,6 @@ module.exports = async (musics) => {
         ele.find("ol").remove();
         ele.find("a").remove();
         ele.find("br").replaceWith("\n");
-        console.log(ele.text())
         return ele.text().split("更多更詳盡歌詞 在").join("");
     })
     .join("\n")
