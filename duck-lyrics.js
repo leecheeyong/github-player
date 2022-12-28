8385487b0d8fbd51e637e2c44d2f1b991d41135c
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-12-28 17:12:41 +0800

    Create duck-lyrics.js

---
+++ duck-lyrics.js
@@ -0,0 +1,54 @@
+const cheerio = require('cheerio');
+const axios = require('duck-duck-scrape');
+const fs = require('fs');
+
+;(async () => {
+    const musics = fs.readdirSync('./music').filter(file => file.endsWith('.mp3'));
+    for(let i in musics) {
+    try {
+    if(fs.existsSync(`./lyrics/${musics[i].slice(0, -4)}.txt`)) throw new Error("Music Exist");
+    console.log(`Fetching lyrics: ${musics[i].slice(0, -4)}`);
+    const name = musics[i].slice(0, -4).toLowerCase()
+    .split("lyrics").join("")
+    .split("official").join("")
+    .split("mv").join("")
+    .split("video").join("")
+    .split("audio").join("")
+    .split("(").join("").split(")").join("")
+    .split("[").join("").split("]").join("")
+    .split("【").join("").split("】").join("")
+    .split("「").join("").split("」").join("")
+    .split("《").join("").split("》").join("")
+    .split("♪").join("").split("動態歌詞").join("")
+    .split("cover").join("").split("mp3").join("")
+    .split("ost").join("").split("歌词版").join("")
+    .split("4k").join("").replace(/\s+/g,' ').trim();
+    
+    const searchResults = (await DDG.search(`site:mojim.com ${name}`)).results.map(e=>e.url);
+        
+    console.log("Scraping Google");
+        
+    if(!searchResults[0]) throw new Error("Google Search not found");
+    
+    console.log("Scraping Mojim", searchResults[0]);
+        
+    const { data } = await axios({ url: `${searchResults[0]}` });
+    const $ = cheerio.load(data);
+    const lyrics = $('dd.fsZx3').toArray().map((x) => {
+        const ele = $(x);
+        ele.find("ol").remove();
+        ele.find("a").remove();
+        ele.find("br").replaceWith("\n");
+        return ele.text().split("更多更詳盡歌詞 在").join("");
+    })
+    .join("\n")
+    .trim();
+    console.log("Processing Lyrics");
+    if(!lyrics) throw new Error("Lyrics not found");
+    fs.writeFileSync(`./lyrics/${musics[i].slice(0, -4)}.txt`, `${lyrics}`);
+    console.log(`Lyrics: ${name}`);
+    }catch(e) {
+        console.log(e)
+    }
+    }
+})();
